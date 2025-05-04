21cdf99dcf45dc3cda3bdbb39d06c82e588fbbd5
jortorres <jordan.torres1@gmail.com>
2025-05-04 11:37:40 -0500

    Added extract_markdown_img.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from textnode import *
 from split_nodes_delim import *
+from extract_markdown_img import *
 
 def main():
     print("Hello World")
@@ -7,8 +8,18 @@ def main():
     new_text_node = TextNode("This is text with a `code block` word", TextType.TEXT)
     print(new_text_node)
 
-    new_nodes = split_nodes_delimiter([new_text_node], "`", TextType.CODE)
-    print(f"Main file: {new_nodes}")
+    #new_nodes = split_nodes_delimiter([new_text_node], "`", TextType.CODE)
+    #print(f"Main file: {new_nodes}")
+
+    text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+    print(extract_markdown_images(text))
+    # [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+
+    text1 = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+    print(extract_markdown_links(text1))
+    # [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+
+
 
 
 main()
