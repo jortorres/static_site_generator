771068c8f817dfbaff40ce61bfd69942cc283291
jortorres <jordan.torres1@gmail.com>
2025-05-06 12:34:52 -0500

    UPDATED ALL FILES

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,20 +1,53 @@
-class HTMLNode:  
-    def __init__(self,tag=None,value=None,children=None,props=None): #string, string, list, dictinary
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
         self.children = children
         self.props = props
-    
+
     def to_html(self):
-        raise NotImplementedError("Subclass used for to html")
-    
+        raise NotImplementedError("to_html method not implemented")
+
     def props_to_html(self):
-        if not self.props:
+        if self.props is None:
             return ""
-        new_string = ""
-        for key, value in self.props.items():
-            new_string += f' {key}="{value}"'
-        return new_string
-    
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
+
     def __repr__(self):
-        return f"Tag: {self.tag} Value: {self.value} Child: {self.children} Props: {self.props}"
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("invalid HTML: no children")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
--- src/main.py
+++ src/main.py
@@ -1,25 +1,9 @@
-from textnode import *
-from split_nodes_delim import *
-from extract_markdown_img import *
+from textnode import TextNode, TextType
+
 
 def main():
-    print("Hello World")
-    
-    new_text_node = TextNode("This is text with a `code block` word", TextType.TEXT)
-    print(new_text_node)
-
-    #new_nodes = split_nodes_delimiter([new_text_node], "`", TextType.CODE)
-    #print(f"Main file: {new_nodes}")
-
-    text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
-    print(extract_markdown_images(text))
-    # [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
-
-    text1 = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
-    print(extract_markdown_links(text1))
-    # [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
-
-
+    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(node)
 
 
 main()
