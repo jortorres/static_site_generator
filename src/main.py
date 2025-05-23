8cdc3759af0ea938f58784cca3d4659c8fefa859
jortorres <jordan.torres1@gmail.com>
2025-05-23 12:30:40 -0500

    Working on copy_direcotry for main()

--- src/main.py
+++ src/main.py
@@ -1,10 +1,32 @@
+import os, shutil
 from textnode import TextNode, TextType
+
 
 
 def main():
     node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(node)
 
+    #check if path exists
+    dir_list = os.listdir(path='.')
+    print(dir_list)
+
+    if os.path.exists("public"): 
+        print("delete the path")
+        dir = "public"
+        location = "public"
+        path = os.path.join(location, dir)
+        shutil.rmtree(path)
+        print("Has been deleted")
+        print("Copy dir")
+        static_dir = "static"
+        shutil.copy(static_dir , dir)
+        print("DIR has been copied and public crated")
 
 
+
+    else:
+        print("Needs to create")
+        os.mkdir("public")
+        
 main()
